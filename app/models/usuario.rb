4454f79b5f94011e06db777db4eaf08cb0b7e427
Alex Aquino <alexqno@gmail.com>
2012-01-07 11:51:08 -0300

    Retirada do método no modelo de usuários. -- Alex Aquino

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -3,13 +3,5 @@ class Usuario < ActiveRecord::Base
   has_and_belongs_to_many :turmas, :join_table => "turmas_alunos", :foreign_key => "aluno_id"
   has_many :turmas, :foreign_key => "professor_id"
   has_many :mensagens
-  
-  def self.existe? usuario
-    if Usuario.where(["fb_id = ?", usuario['id']])
-      true
-    else
-      false
-    end
-  end
 
 end
